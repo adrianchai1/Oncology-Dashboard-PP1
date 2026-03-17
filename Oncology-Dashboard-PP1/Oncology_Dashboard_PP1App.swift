c42d5e2af73f99d06d311088d645265b560b9242
oim1 <oim.oom.imo@gmail.com>
2026-03-18 09:42:20 +1100

    Initial Commit

---
+++ Oncology-Dashboard-PP1/Oncology_Dashboard_PP1App.swift
@@ -0,0 +1,17 @@
+//
+//  Oncology_Dashboard_PP1App.swift
+//  Oncology-Dashboard-PP1
+//
+//  Created by Adrian Chai on 18/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct Oncology_Dashboard_PP1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

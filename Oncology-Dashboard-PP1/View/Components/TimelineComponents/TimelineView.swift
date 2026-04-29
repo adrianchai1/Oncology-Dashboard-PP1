3427840649f07d17661eea6b54ef140a5adbac56
zRanqa <jonnoach@gmail.com>
2026-04-29 12:34:59 +1000

    Added timeline cycles to the main view to display

--- Oncology-Dashboard-PP1/View/Components/TimelineComponents/TimelineView.swift
+++ Oncology-Dashboard-PP1/View/Components/TimelineComponents/TimelineView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct TimelineView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal) {
+            LazyHStack {
+                TimelineCycle()
+                TimelineCycle()
+                TimelineCycle()
+                TimelineCycle()
+                TimelineCycle()
+            }
+        }
     }
 }
 

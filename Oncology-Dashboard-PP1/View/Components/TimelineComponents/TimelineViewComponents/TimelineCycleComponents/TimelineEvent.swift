680fe87adaf9c0947c7a180c0f58279f067ba41a
zRanqa <95194338+zRanqa@users.noreply.github.com>
2026-04-30 10:26:50 +1000

    Timeline Functionality (#3)
    * Added timeline component, added timelineEvent struct and view. Made timeline event view positioned based on the cycle date
    
    * Added timeline cycles to the main view to display
    
    * Added line on timeline cycle to connect timeline events to the timeline
    
    * Added timeline bar
    
    * Attempted to make scrollview auto scroll to last cycle

---
+++ Oncology-Dashboard-PP1/View/Components/TimelineComponents/TimelineViewComponents/TimelineCycleComponents/TimelineEvent.swift
@@ -0,0 +1,114 @@
+//
+//  TimelineEvent.swift
+//  Oncology-Dashboard-PP1
+//
+//  Created by Jonathan Dummett on 29/4/2026.
+//
+
+import Foundation
+import SwiftUI
+
+// TODO: Need to add cases for blood pressure, sleep etc. Things doctors could flag as a problem and put it on the timeline view.
+enum EventID: Codable, CaseIterable {
+    case chemotherapy
+    case appointment
+    case emergency
+    case other
+}
+
+
+struct TimelineEvent: Identifiable, Codable {
+    var id: UUID = UUID()
+    var eventId: EventID
+    var date: Date
+    var notes: String
+    var doctorId: UUID = UUID()
+    var title: String?
+    
+    var getTitle: String {
+       if title != nil {
+            return title!
+        }
+        
+        switch eventId {
+        case .chemotherapy:
+            return "Chemotherapy"
+        case .appointment:
+            return "Appointment"
+        case .emergency:
+            return "Emergency"
+        case .other:
+            return "Other"
+        }
+    }
+    
+    var getIcon: String {
+        switch eventId {
+        case .chemotherapy:
+            return "pills"
+        case .appointment:
+            return "calendar"
+        case .emergency:
+            return "heart"
+        case .other:
+            return "questionmark"
+        }
+    }
+    
+    var getColor: Color {
+        switch eventId {
+        case .chemotherapy:
+            return .green.opacity(0.5)
+        case .appointment:
+            return .blue.opacity(0.5)
+        case .emergency:
+            return .red.opacity(0.5)
+        case .other:
+            return .gray.opacity(0.5)
+        }
+    }
+}
+
+func getTestTimelineEvent(startDate: Date?, forceDate: Date? = nil) -> TimelineEvent {
+    let randomEvent = EventID.allCases.randomElement()!
+    
+    
+    var date = Date()
+    if startDate != nil {
+        date = Calendar.current.date(byAdding: .day, value: Int.random(in: 0...13), to: startDate!) ?? Date()
+    }
+    if forceDate != nil {
+        date = forceDate!
+    }
+    
+    switch randomEvent {
+    case .chemotherapy:
+        return TimelineEvent(
+            eventId: .chemotherapy,
+            date: date,
+            notes: "Test Event"
+        )
+        
+    case .appointment:
+        return TimelineEvent(
+            eventId: .appointment,
+            date: date,
+            notes: "Test Event"
+        )
+        
+    case .emergency:
+        return TimelineEvent(
+            eventId: .emergency,
+            date: date,
+            notes: "Test Event"
+        )
+    case .other:
+        return TimelineEvent(
+            eventId: .other,
+            date: date,
+            notes: "Test Event",
+            title: "Test Title"
+        )
+    }
+}
+
